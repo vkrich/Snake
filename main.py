5c21452d3c8ade51cebf5e69e285aac826aa43aa
vkrich <vladislav.krichevsky@mail.ru>
2020-11-04 11:40:45 +0000

    Add some comments

--- main.py
+++ main.py
@@ -3,16 +3,18 @@ import sys
 import random
 import sqlite3
 
-#colors
+# colors
 WHITE = (255, 255, 255)
 BLUE = (204, 255, 255)
 BLACK = (0, 0, 0)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
-
+# all is divided by blocksize
 BLOCK_SIZE = 25
+# size of playground
 size = (BLOCK_SIZE*BLOCK_SIZE, BLOCK_SIZE*BLOCK_SIZE)
 
+# inheritance from Sprite for image on background
 class Background(pygame.sprite.Sprite):
   def __init__(self, image_file, location):
     pygame.sprite.Sprite.__init__(self) 
@@ -22,56 +24,64 @@ class Background(pygame.sprite.Sprite):
 
 
 def table_creation():
-  conn = sqlite3.connect("Snake.db") 
-  # или :memory: чтобы сохранить в RAM
-  cursor = conn.cursor()
-  
-  # Создание таблицы в БД для хранения очков пользователя
+  # connect to db
+  conn = sqlite3.connect("Snake.db")   
+  cursor = conn.cursor()  
+  # Create table for saving players scores
   cursor.execute("""CREATE TABLE players (
                   player_id INTEGER PRIMARY KEY AUTOINCREMENT,
                   player_name text NOT NULL,
                   scores INTEGER 
                 );
                 """)
+  # commit changes in db        
   conn.commit()  
 
 
 def spam_apple(snake, stones):
+  '''Function generate coords for apple'''
   a = random.randrange(0, size[0]-BLOCK_SIZE, BLOCK_SIZE)
   b = random.randrange(0, size[1]-BLOCK_SIZE, BLOCK_SIZE)
+  # a, b mod BLOCK_SIZE == 0
+  # a, b < size[0], size[1]
   if (a, b) in snake or (a, b) in stones:
     a, b = spam_apple(snake, stones)
+    # generate a, b again
   return (a, b)
 
 
 def game(screen):  
+  '''Main game function'''
+  # Create coords for snake, stones and apple
   x = random.randrange(BLOCK_SIZE, size[0]-BLOCK_SIZE*2, BLOCK_SIZE)
   y = random.randrange(BLOCK_SIZE, size[1]-BLOCK_SIZE*2, BLOCK_SIZE)
-  snake = [(x, y)]
+  snake = []
+  snake.append((x, y))
   snakesize = len(snake)
   stones = []
   for i in range(250, 301, BLOCK_SIZE):
     for j in range(250, 301, BLOCK_SIZE):
       stones.append((i, j))
+
   apple_x, apple_y = spam_apple(snake, stones)
-  
+  # variables for texting scores
   scores_text_font = pygame.font.SysFont('serif', 36)
   scores_points = 0
-
-  clock = pygame.time.Clock()
+  # for moving logic
   direction_moving = {'W': True, 'A': True, 'S': True, 'D': True}
   dx, dy = 0, 0
+  clock = pygame.time.Clock()
 
   while True:                 
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
-          sys.exit()
-    keys = pygame.key.get_pressed()
-
-    BackGround = Background('grass.png', [0,0])
+        sys.exit()    
+    # Background drawing from file
+    BackGround = Background('grass.png', [0, 0])
     screen.blit(BackGround.image, BackGround.rect)
-    #snake = [(i, j) for i, j in snake]    
-    
+     
+    keys = pygame.key.get_pressed()
+    # Analysing pressed button on keyboard
     if keys[pygame.K_d] and direction_moving['D']:
       direction_moving = {'W': True, 'A': False, 'S': True, 'D': True}
       dx = BLOCK_SIZE
@@ -95,7 +105,7 @@ def game(screen):
     snake.append((x, y))
     #delete last block in snake
     snake = snake[-snakesize:]
-
+    # snake teleporting cycle
     for i in range(len(snake)):
       if snake[i][0] > size[0] - BLOCK_SIZE:          
         snake[i] = (0, snake[i][1])
@@ -112,7 +122,7 @@ def game(screen):
       if snake[i][1] < 0:          
         snake[i] = (snake[i][0], size[1]- BLOCK_SIZE)
         y = size[1] - BLOCK_SIZE
-
+    # snake ate apple
     if snake[-1][0] == apple_x and snake[-1][1] == apple_y:
       snakesize+=1
       scores_points+=1
@@ -120,18 +130,20 @@ def game(screen):
     
     if len(snake) != len(set(snake)) or set(snake) & set(stones):
       return scores_points
-    
+    # draw stones, snake, head snake, apple
     [pygame.draw.rect(screen, BLACK, (i, j, BLOCK_SIZE, BLOCK_SIZE)) for i, j in stones]
     [pygame.draw.rect(screen, BLACK, (i, j, BLOCK_SIZE, BLOCK_SIZE)) for i, j in snake]
     pygame.draw.rect(screen, WHITE, (snake[-1][0], snake[-1][1], BLOCK_SIZE, BLOCK_SIZE))
     pygame.draw.rect(screen, RED, (apple_x, apple_y, BLOCK_SIZE, BLOCK_SIZE))
-    text_scores = scores_text_font.render(f"Scores: {scores_points}", 0, RED) 
+
+    text_scores = scores_text_font.render(f"Scores: {scores_points}", 0, RED)
     screen.blit(text_scores, (10, 25))
-    clock.tick(10)
+    clock.tick(5)
+    #refresh display
     pygame.display.flip()
 
 
-def user_creation(screen):
+def user_creation(screen, scores):
   input_text = ""
   input_active = True
   while True:    
@@ -144,7 +156,7 @@ def user_creation(screen):
       elif event.type == pygame.KEYDOWN and input_active:
         if event.key == pygame.K_RETURN:
           input_active = False
-          # GET input text in DB with scores
+          # GET input text in DB with scores, input_text
         elif event.key == pygame.K_BACKSPACE:
           input_text =  input_text[:-1]
         else:
@@ -161,9 +173,9 @@ def user_creation(screen):
     screen.blit(text_surf, (10, 450))
     pygame.display.flip()
 
-
+# start
 pygame.init()
 screen = pygame.display.set_mode(size)
-game(screen)
-user_creation(screen)
+taken_scores = game(screen)
+user_creation(screen, taken_scores)
 pygame.quit()
